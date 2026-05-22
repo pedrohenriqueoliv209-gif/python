007c32fb408c4c132fe9899df582f5a7f4470e55
pedrohenriqueoliv209-gif <pedrohenriqueoliv209@gmail.com>
2026-05-22 15:00:42 -0300

    primeira aula python

---
+++ teste.py
@@ -0,0 +1,14 @@
+print("Olá Mundo,")
+# criando variaveis
+nome = "mariana"
+idade = "16"
+print(nome)
+print(idade)
+#operações básicas
+soma = 10 + 5
+multiplicação = 4 * 5
+divisão = 20 / 4
+print("soma", soma)
+print("multiplicação", multiplicação)
+print("divisão", divisão)
+
