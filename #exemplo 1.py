c8782d548dcf14dca9cb539b54098e8e3a42f86f
pedrohenriqueoliv209-gif <pedrohenriqueoliv209@gmail.com>
2026-06-12 14:50:13 -0300

    aula 2

---
+++ #exemplo 1.py
@@ -0,0 +1,30 @@
+#exemplo 1
+print("olá")
+a = "carro"
+b = "velho"
+print(a+b)
+print(a+ " " +b)
+
+
+#exemplo 2
+print("olá")
+a = 2026
+b = 53
+print(a+b)
+
+
+#exemplo 3 
+print("olá")
+a = 2026
+b = 53
+print(a**b)
+
+
+
+#exemplo 4
+
+nota = "I"
+if nota =="MB" :
+ print("aprovado")
+else:
+ print("reprovado")
