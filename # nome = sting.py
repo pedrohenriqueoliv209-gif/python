3ae0ab2667d8e5a3565e1c109f32994fc08b8544
pedrohenriqueoliv209-gif <pedrohenriqueoliv209@gmail.com>
2026-06-19 14:19:23 -0300

    aula 3

---
+++ # nome = sting.py
@@ -0,0 +1,23 @@
+# nome = sting
+# idade = int
+# altura = float
+#string são caracteres/textos que são apenas textos não será usado para fazer conta
+# int vem de número inteiro, será usada para calculos etc.
+#float é número quebrado exemplo: 1.0 1.60
+
+nome = "pedro" #texto
+idade = 17 #Número
+meses = idade * 12
+altura = 1.90 #float (ponto flutuante)
+print("meu nome é", nome, " e eu tenho", idade, "anos ou", meses, "meses", "ou", idade *365 ,"dias", "ou", idade / 100, "centenario.")
+
+
+
+
+
+
+
+
+
+
+
